5680fc7ab72655a4868bfdbfada751e71d4473fa
Toni D <tonidy@outlook.com>
2024-04-19 17:39:02 +0700

    Add prompt menu

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const io = require('socket.io-client');
+const readline = require('readline');
 
+// URL: https://vidio.stg.starcade.co
+// token: UNITY-TsszHgZ6dULmPzR
 // Connect to the WebSocket server
-const socket = io('https://vidio.stg.starcade.co', {
+const socket = io('http://localhost:3000', {
   query: {
-    token: 'UNITY-TsszHgZ6dULmPzR' // Provide the token for authentication
+    token: 'UNITY' // Provide the token for authentication
   }
 });
+
+// Create interface for input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Prompt menu function
+function promptMenu() {
+  rl.question('Choose an option:\n1. Send "hello" message\n', (option) => {
+    if (option.trim() === '1') {
+      socket.emit('hello', { message: `Hello from client ${new Date().getTime()}` });
+    } else {
+      console.log('Invalid option.');
+    }
+    promptMenu(); // Call itself recursively for a forever loop
+  });
+}
 
 // Handle connection event
 socket.on('connect', () => {
@@ -13,9 +34,9 @@ socket.on('connect', () => {
 
   // Send a 'hello' message to the server
   socket.emit('hello', { message: 'Hello from client' });
-  //socket.emit('spin');
-//   socket.emit('spin', { message: 'Spin' });
-//   socket.emit('class', { message: 'Class' });
+
+  // Start prompt menu
+  promptMenu();
 });
 
 // Handle 'hello' message from the server
@@ -23,17 +44,14 @@ socket.on('hello', (data) => {
   console.log('Received hello message from server:', data);
 });
 
-// Handle 'spin' message from the server
-socket.on('spin', (data) => {
-  console.log('Received spin message from server:', data);
-});
-
-// Handle 'class' message from the server
-socket.on('class', (data) => {
-  console.log('Received class message from server:', data);
-});
-
 // Handle disconnection event
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
+});
+
+// Listen for Ctrl + C to exit
+rl.on('SIGINT', () => {
+  console.log('\nExiting...');
+  rl.close();
+  process.exit();
 });
